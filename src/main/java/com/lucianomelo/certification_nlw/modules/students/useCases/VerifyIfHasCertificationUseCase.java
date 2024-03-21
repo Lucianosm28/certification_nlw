fb81603e0a41b46c7b5987850823ad20ad94c197
unknown <lsome@live.com>
2024-03-21 01:34:02 -0300

    Aula 02, banco de dados e Docker

--- src/main/java/com/lucianomelo/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/com/lucianomelo/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,13 +1,20 @@
 package com.lucianomelo.certification_nlw.modules.students.useCases;
 
 import com.lucianomelo.certification_nlw.modules.students.dto.VerifyIfHasCertificationDTO;
+import com.lucianomelo.certification_nlw.modules.students.repositories.CertificationStudentEntityRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
 
+    @Autowired   //Para utilizar um componente gerenciado pelo Spring coloca ele
+    private CertificationStudentEntityRepository certificationStudentEntityRepository;
+
     public boolean execute(VerifyIfHasCertificationDTO dto) {
-        if (dto.getEmail().equals("messi@exemplo.com") && dto.getTechnology().equals("JAVA")) {
+        var result = this.certificationStudentEntityRepository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+        if (!result.isEmpty()) {
             return true;
         }
     return false;
